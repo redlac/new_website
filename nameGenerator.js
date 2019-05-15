99f259e15aea1371ef133f9fea84b79407cabd01
redlac <runeman2@gmail.com>
2019-05-15 12:57:14 -0600

    Update nameGenerator.js

--- nameGenerator.js
+++ nameGenerator.js
@@ -78,7 +78,7 @@ document.getElementById("createFirstLastName").onclick = function(){
 document.getElementById("shuffleName").onclick = function(){
   if (document.getElementById("result")){
     let name = document.getElementById("result").innerHTML;
-    console.log(name);
+    console.log(name.length);
     let nameArray = name.split("");
     console.log(nameArray);
     let nameLength = name.length;
